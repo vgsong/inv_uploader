9735049d1cbf415b7fb038436896c9a0b9e967a4
vgsong <vgsong@proton.me>
2025-06-15 16:05:50 -0700

    push push

--- main.py
+++ main.py
@@ -20,25 +20,46 @@ class InvoiceUploader:
         load_dotenv()
         
         self.mdir = os.getenv('MDIR')
+        self.data_dir = os.path.join(self.mdir, 'data')
+        self.wo_filename = 'wo.csv'
         
+        self.wo = None
+        self.time_stamp = datetime.now()
         
         self.main_menu = {1 : ['LAUNCH BOT', 'self.start_upload'],
-                          2: ['OPEN DIR', self.open_mdir],
-                          0: ['QUIT', exit],
+                          2 : ['OPEN DIR', self.open_mdir],
+                          3 : ['REFRESH WO', self.load_wo],
+                          0 : ['QUIT', exit],
                           }
 
 
+    def load_wo(self):
+        result = []
+        with open(os.path.join(self.data_dir, self.wo_filename), 'r', encoding='utf-8') as cf:
+            data = csv.reader(cf)
+            for x in list(data)[1:]:
+                result.append(x)
+        self.wo = result
+        return 
+
     def start_menu(self):
         while True:
+            
+            print('wo loaded: {}'.format(len(self.wo)))
+            print('Hello! Today is {}'.format(self.time_stamp.strftime('%Y %m %d')))
+            
             print('----- MAIN MENU ------')
             for k, v in self.main_menu.items():
                 print('{}: {}'.format(k, v[0]))
+            
+                
+                
             user_input = input('Please enter index to select:\n')
             try:
                 self.main_menu[int(user_input)][1]()
             except Exception as e:
                 print(e)
-
+                
     def open_mdir(self):
         os.startfile(os.path.join(self.mdir))
         return
